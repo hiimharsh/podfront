b80784ee597cd9d4eeee0309110088291e86635f
Midhet <midhetfatema94@gmail.com>
2016-10-08 00:00:13 +0530

    completed

--- js/controllers.js
+++ js/controllers.js
@@ -91,12 +91,15 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
   $scope.pods = versions;
   console.log ($scope.pods);
 
-  var githubHeaderRe = /https/g;
-  var gitSymbolsRe = /[://]/g;
-  var githubRe = /github.com/g;
-  var dotGitRe = /.git/g;
+  // var githubHeaderRe = /https/g;
+  // var gitSymbolsRe = /[://]/g;
+  // var githubRe = /github.com/g;
+  // var dotGitRe = /.git/g;
+
+  var specRe = /s[.]\w+/g;
 
   var cocoapodApi = "";
+  var specData = [];
   for (var i = 0; i < $scope.pods.length; i++) {
     cocoapodApi = "http://search.cocoapods.org/api/v1/pods.picky.hash.json?query=name:"+ $scope.pods[i].item+"&amount=1&start-at=0";
     console.log("cocoapodApi: ", cocoapodApi)
@@ -117,7 +120,13 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
 
       NavigationService.getGithubApi(githubApiUrl, function(data) {
 
-        console.log("data in github: ", data)
+        // console.log("data in github: ", data)
+        specData = data.split('\n')
+        specData = specData.filter(function(item) {
+          return specRe.test(item) && !hashRe.test(item)
+        })
+        console.log("specData: ", specData)
+        formatResults(specData)
 
       })
 
@@ -129,6 +138,96 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
   // });
 
   };
+
+  function formatResults(data) {
+    var results = {};
+    console.log("data in formatResults: ", data)
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].includes("s.name")) {
+        var name = makeSubstring(data[i])
+        console.log("pod name: ", name)
+        results["name"] = name
+      }
+      else if (data[i].includes("s.homepage")) {
+        var link = makeSubstring(data[i])
+        console.log("pod link: ", link)
+        results["link"] = link
+      }
+      else if (data[i].includes("s.summary")) {
+        var description = makeSubstring(data[i])
+        console.log("pod description: ", description)
+        results["description"] = description
+      }
+      else if (data[i].includes("s.platform")) {
+        if (data[i].includes(":ios")) {
+           var ios = makeSubstring(data[i])
+           console.log("pod ios: ", ios)
+           results["ios"] = ios
+        }
+        else if (data[i].includes(":watchos")) {
+           var watchos = makeSubstring(data[i])
+           console.log("pod watchos: ", watchos)
+           results["watchos"] = watchos
+        }
+        else if (data[i].includes(":tvos")) {
+           var tvos = makeSubstring(data[i])
+           console.log("pod tvos: ", tvos)
+           results["tvos"] = tvos
+        }
+        else if (data[i].includes(":macos")) {
+           var macos = makeSubstring(data[i])
+           console.log("pod macos: ", macos)
+           results["macos"] = macos
+        }
+      }
+      else if (data[i].includes("s.ios.deployment_target")) {
+        var ios = makeSubstring(data[i])
+        console.log("pod ios: ", ios)
+        results["ios"] = ios
+      }
+      else if (data[i].includes("s.watchos.deployment_target")) {
+        var watchos = makeSubstring(data[i])
+        console.log("pod watchos: ", watchos)
+        results["watchos"] = watchos
+      }
+      else if (data[i].includes("s.tvos.deployment_target")) {
+        var tvos = makeSubstring(data[i])
+        console.log("pod tvos: ", tvos)
+        results["tvos"] = tvos
+      }
+      else if (data[i].includes("s.macos.deployment_target")) {
+        var macos = makeSubstring(data[i])
+        console.log("pod macos: ", macos)
+        results["macos"] = macos
+      }
+    }
+    console.log("result:", results)
+    $scope.podResults.push(results)
+
+  }
+
+  function makeSubstring(text) {
+
+    // console.log("last index: ", text.lastIndexOf('"') - 1)
+    // var startIndex = text.lastIndexOf('"') - 1 || text.lastIndexOf("'") - 1
+    // var startPos = text.substring(text.startIndex + 1)
+    // var endPos = text.lastIndexOf('"') || text.lastIndexOf("'")
+    var splitResultOne = text.split("'")
+    var splitResultTwo = text.split('"')
+    if (splitResultOne[1]) {
+      console.log("splitResult: ", splitResultOne[1])
+      var result = splitResultOne[1]
+      console.log("substring result: ", result)
+      return result
+    }
+    else {
+      console.log("splitResult: ", splitResultTwo[1])
+      var result = splitResultTwo[1]
+      console.log("substring result: ", result)
+      return result
+    }
+
+  }
 
 
   // $scope.upload = function(files) {
